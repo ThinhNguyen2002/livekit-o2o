26ab68f74bd4a6be510e565020387223c7e7820b
Thinh Nguyen <thinh.nguyen@moovapp.vn>
2025-01-20 22:02:18 +0700

    Add console log to Home page for debugging purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Container, Flex, Kbd, Link, Separator, Text } from "@radix-ui/themes";
 import Image from "next/image";
 
 export default function Home() {
+  console.log("Home page");
   return (
     <main className="flex min-h-screen flex-col items-center gap-12 p-10 sm:p-24">
       <Container size="1">
